f184d9f6ff9875c90ac8e89c3909b3ee8ea9b528
Ahmed Ait Lfdil <aait-lfd@e2r8p9.1337.ma>
2023-06-15 13:58:54 +0100

    add new utils

--- includes/utils.h
+++ includes/utils.h
@@ -6,15 +6,18 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 15:47:10 by aait-lfd          #+#    #+#             */
-/*   Updated: 2023/06/12 15:58:19 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/06/15 13:30:19 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
+int		is_char_quoted(char *str, char *pos);
 char	*get_prompt(void);
 int		is_blank(char *str);
-void	check_quotes(char *str);
+char	check_quotes(char *str);
 char	*join_strings(char **arr, int size, char *sep);
+void	trow_error(char *msg, int err_code);
+
 #endif
