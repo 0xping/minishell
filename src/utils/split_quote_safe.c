8119212cba945cb4183c443cb76d6b414630a757
Ahmed Ait Lfdil <aait-lfd@e2r10p9.1337.ma>
2023-07-06 11:30:22 +0100

    fix split bug

--- src/utils/split_quote_safe.c
+++ src/utils/split_quote_safe.c
@@ -6,7 +6,7 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 17:19:34 by aait-lfd          #+#    #+#             */
-/*   Updated: 2023/06/22 17:19:38 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/07/06 11:29:08 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,12 @@ static size_t	count_chunks(char *cmd, char *dels, bool handle_empty_str)
 	result = 0;
 	while (cmd[i])
 	{
-		if (handle_empty_str)
-			cond = ft_strchr(dels, cmd[i]) && !is_char_quoted(cmd, &cmd[i]);
+		if (handle_empty_str && !is_char_quoted(cmd, &cmd[i]))
+			cond = ft_strchr(dels, cmd[i]);
 		else
 			cond = (i == 0 && !ft_strchr(dels, cmd[i])) || (ft_strchr(dels,
-						cmd[i]) && !ft_strchr(dels, cmd[i + 1]) && cmd[i + 1]);
+						cmd[i]) && !ft_strchr(dels, cmd[i + 1]) && cmd[i + 1]
+					&& !is_char_quoted(cmd, &cmd[i]));
 		if (cond)
 			result++;
 		i++;
@@ -77,10 +78,12 @@ static void	fill_arr(size_t words, char *s, char *dels, char ***result)
 	start = 0;
 	while (i < words)
 	{
-		while (ft_strchr(dels, s[start]) && s[start])
+		while (s[start] && ft_strchr(dels, s[start]) && !is_char_quoted(s,
+				&s[start]))
 			start++;
 		end = start;
-		while (!ft_strchr(dels, s[end]) && s[end])
+		while (s[end] && (!ft_strchr(dels, s[end]) || is_char_quoted(s,
+					&s[end])))
 			end++;
 		(*result)[i] = ft_substr(s, start, end - start);
 		start = end;
@@ -96,6 +99,7 @@ char	**split_quote_safe(char *cmd, char *dels, bool handle_empty_str)
 	if (!cmd)
 		return (0);
 	slices = count_chunks(cmd, dels, handle_empty_str);
+	printf("-->%zu\n", slices);
 	result = ft_calloc(slices + 1, sizeof(char *));
 	if (result)
 	{
