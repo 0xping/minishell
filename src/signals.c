3f7f54c474cda5d1156707772140d212bcbfcaef
Ahmed AITLFDIL <afadilahmad7@gmail.com>
2023-07-27 20:44:39 +0100

    move file from src/executer/ to src/

--- src/signals.c
+++ src/signals.c
@@ -6,34 +6,27 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 17:04:37 by aait-lfd          #+#    #+#             */
-/*   Updated: 2023/07/10 17:57:29 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/07/27 20:30:03 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
+#include "../includes/minishell.h"
 
 void	sigint_handler(int sigint)
 {
-
-	// rl_on_new_line, rl_replace_line, rl_redisplay
-	// tcsetattr, tcgetattr
-	//ft_putstr_fd("\n", 1);
+	ft_putstr_fd("\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 	(void)sigint;
 }
 
-
 void	set_signal_printing(void)
 {
-	struct termios	ter_attr;
-	int				fd;
+	struct termios	new_term;
 
-	fd = STDIN_FILENO;
-	tcgetattr(fd, &ter_attr);
-	//change terminal attributes
-
-	tcsetattr(fd, TCSANOW, &ter_attr);
+	tcgetattr(0, &new_term);
+	// Disable printing signals (disable ISIG flag) and disable character echoing (disable ECHO flag)
+	new_term.c_lflag &= ~(ISIG | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
 }
-
