4d5e6164f54c9040c09add54ebb8e13bd4274122
Ahmed AITLFDIL <afadilahmad7@gmail.com>
2023-08-04 00:50:50 +0100

    add new utils

---
+++ src/utils/push_char_to_str.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_char_to_str.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 04:41:53 by aait-lfd          #+#    #+#             */
+/*   Updated: 2023/08/04 00:50:30 by aait-lfd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	push_char_to_str(char **str, char c)
+{
+	char	*c_as_str;
+	char	*tmp;
+
+	c_as_str = char_to_str(c);
+	tmp = *str;
+	*str = ft_strjoin(*str, c_as_str);
+	ft_free(tmp);
+	ft_free(c_as_str);
+}
