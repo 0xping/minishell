9e4a85b6f4e501583db20c328367c42b62b30a8b
Ahmed AITLFDIL <afadilahmad7@gmail.com>
2023-08-10 03:18:14 +0100

    check maximum here-document count

--- includes/executer.h
+++ includes/executer.h
@@ -6,7 +6,7 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 16:20:25 by aait-lfd          #+#    #+#             */
-/*   Updated: 2023/07/10 17:55:35 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/08/10 02:37:24 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 void	executer(t_list *cmd_list);
 void	set_redirections(t_list *cmd_list);
 void	set_heredocs(t_list *cmd_list);
+void	check_16_heredocs(t_list *cmd_list);
 void	sigint_handler(int sigint);
 
 #endif
