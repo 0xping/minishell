27565ef82fdfc901be7bf571a602016fef6ddf79
Ahmed AITLFDIL <afadilahmad7@gmail.com>
2023-08-11 02:00:46 +0100

    remove BG colors

--- includes/inc.h
+++ includes/inc.h
@@ -6,7 +6,7 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 21:32:58 by aait-lfd          #+#    #+#             */
-/*   Updated: 2023/08/10 03:43:00 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/08/10 23:45:42 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,9 @@
 # define COLOR_WHITE "\x1b[37m"
 # define COLOR_RESET "\x1b[0m"
 
-# define BG_COLOR_BLACK "\x1b[40m"
-# define BG_COLOR_RED "\x1b[41m"
-# define BG_COLOR_GREEN "\x1b[42m"
-# define BG_COLOR_YELLOW "\x1b[43m"
-# define BG_COLOR_BLUE "\x1b[44m"
-# define BG_COLOR_MAGENTA "\x1b[45m"
-# define BG_COLOR_CYAN "\x1b[46m"
-# define BG_COLOR_WHITE "\x1b[47m"
-
 // Text style macros
 # define TEXT_BOLD "\x1b[1m"
-# define TEXT_UNDERLINE "\x1b[4m"
-# define TEXT_REVERSE "\x1b[7m"
 
-# define REVERSE_TEXT "\x1b[7m"
 # define RESET_ALL "\x1b[0m"
 
 # include <fcntl.h>
