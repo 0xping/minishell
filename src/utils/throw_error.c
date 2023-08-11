b88461128d206741fb3267affb7e14373a762def
Ahmed AITLFDIL <afadilahmad7@gmail.com>
2023-08-11 05:22:02 +0100

    norm fix

--- src/utils/throw_error.c
+++ src/utils/throw_error.c
@@ -6,14 +6,15 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 16:25:25 by aait-lfd          #+#    #+#             */
-/*   Updated: 2023/08/06 21:32:22 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/08/11 05:21:25 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/inc.h"
 
-void	throw_error(char *msg, int err_code) {
-  ft_putstr_fd("minishell: ", 2);
-  ft_putendl_fd(msg, 2);
-  g_vars.exit_status = err_code;
+void	throw_error(char *msg, int err_code)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putendl_fd(msg, 2);
+	g_vars.exit_status = err_code;
 }
