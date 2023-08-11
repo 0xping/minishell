29ffbd69cb14787deef067b272cd2372a61e4bd1
Ahmed AITLFDIL <afadilahmad7@gmail.com>
2023-08-11 03:27:38 +0100

    update pwd

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 03:00:04 by m-boukel          #+#    #+#             */
-/*   Updated: 2023/08/06 21:32:22 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/08/11 03:26:42 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ void	ft_pwd(int fd)
 {
 	char	*path;
 
-	path = 0;
-	path = getcwd(path, 0);
+	path = getcwd(0, 0);
 	ft_putendl_fd(path, fd);
 	free(path);
 }
