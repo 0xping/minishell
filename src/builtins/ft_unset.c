3a6d757bbddebd22ec183ecf251282d990f0d10c
Ahmed Ait Lfdil <aait-lfd@c3r9p2.1337.ma>
2023-08-26 16:13:45 +0100

    unset: fix a bug

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,71 +6,58 @@
 /*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 11:17:15 by m-boukel          #+#    #+#             */
-/*   Updated: 2023/08/25 16:42:40 by aait-lfd         ###   ########.fr       */
+/*   Updated: 2023/08/26 15:45:56 by aait-lfd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/inc.h"
 
-int	unset_one_arg(t_list *i, char **cmd, int j)
+void	del_env_node(t_list *del)
 {
-	t_list	*tmp;
+	t_list	**head;
 
-	if (!cmd[1])
-		return (1);
-	if (!ft_strcmp(((t_env *)i->content)->name, cmd[j]))
-	{
-		tmp = i;
-		g_vars.env = i->next;
-		free(tmp->content);
-		free(tmp);
-		if (!cmd[j + 1])
-			return (1);
-	}
-	return (0);
+	head = &g_vars.env;
+	if (*head == 0 || del == 0)
+		return ;
+	if (*head == del)
+		*head = del->next;
+	if (del->next)
+		(del->next)->prev = del->prev;
+	if (del->prev)
+		(del->prev)->next = del->next;
+	free(((t_env *)del->content)->name);
+	free(((t_env *)del->content)->value);
+	free(del->content);
+	free(del);
 }
 
-int	_unset(t_list *i, char **cmd, int j)
+void	unset_var(char *var)
 {
-	t_list	*tmp;
+	t_list	*i;
+	t_env	*content;
 
-	if (!ft_strcmp(((t_env *)i->content)->name, cmd[j]))
+	i = g_vars.env;
+	while (i)
 	{
-		tmp = i;
-		if (i->prev)
-			i->prev->next = i->next;
-		if (i->next)
-			i->next->prev = i->prev;
-		free(((t_env *)tmp->content)->value);
-		free(((t_env *)tmp->content)->name);
-		free(tmp->content);
-		free(tmp);
-		return (1);
+		content = (t_env *)i->content;
+		if (!ft_strcmp(get_env_by_name(var)->name, content->name))
+		{
+			del_env_node(i);
+			break ;
+		}
+		i = i->next;
 	}
-	return (0);
 }
 
 void	ft_unset(char **cmd)
 {
-	t_list	*i;
-	int		j;
+	int	i;
 
+	i = 0;
 	g_vars.exit_status = 0;
-	j = 0;
-	while (cmd[++j])
+	while (cmd[++i])
 	{
-		i = g_vars.env;
-		if (i)
-		{
-			i->prev = NULL;
-			if (unset_one_arg(i, cmd, j))
-				return ;
-			while (i)
-			{
-				if (_unset(i, cmd, j))
-					break ;
-				i = i->next;
-			}
-		}
+		if (get_env_by_name(cmd[i]))
+			unset_var(cmd[i]);
 	}
 }
